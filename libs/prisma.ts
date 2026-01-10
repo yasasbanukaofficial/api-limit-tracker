00bf90ec1092fae73e816becf74b10b294b6be18
yasas banuka <yixbinfo@gmail.com>
2026-01-10 18:15:20 +0530

    feat: add Prisma client setup with PostgreSQL adapter

---
+++ libs/prisma.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client/extension";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+export const prisma = new PrismaClient({ adapter });
